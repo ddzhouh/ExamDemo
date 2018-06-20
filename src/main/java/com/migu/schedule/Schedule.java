8f5fa7eb18a6eca6576feef1c762d61edd06a689
ddzhouh@isoftstone.com <ddzhouh@isoftstone.com>
2018-06-20 22:17:04 +0800

    提交代码

--- src/main/java/com/migu/schedule/Schedule.java
+++ src/main/java/com/migu/schedule/Schedule.java
@@ -1,210 +1,167 @@
 package com.migu.schedule;
 
 
-
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
 import com.migu.schedule.constants.ReturnCodeKeys;
+import com.migu.schedule.info.Task;
 import com.migu.schedule.info.TaskInfo;
+
 
 /*
 *类名和方法不能修改
  */
 public class Schedule {
-
-    TaskInfo taskinfo = new TaskInfo();
-    int nodelist[] = null;
-    int tasklist[] = null;
-    int consumptionlist[] = null;
+    /**
+     * 注册节点Set
+     */
+    public static HashSet<Integer> registerNodeSet = new HashSet<Integer>();
+    
+    /**
+     * 任务挂起队列
+     */
+    public static Queue<Task> waitQueue = new LinkedList<Task>();
+    
+    /**
+     * 任务分配数组
+     */
+    public static Map<Integer, LinkedList<Task>> RuningTask = new HashMap<Integer, LinkedList<Task>>();
+    
     public int init() {
-    	try{
-    		taskinfo.setNodeId(0);
-    		taskinfo.setTaskId(0);
-    		return ReturnCodeKeys.E001;
-    	}
-    	catch(Exception e){
-    		return -1;
-    	}
+        
+        registerNodeSet = new HashSet<Integer>();
+        waitQueue = new LinkedList<Task>();
+        RuningTask = new HashMap<Integer, LinkedList<Task>>();
+        return ReturnCodeKeys.E001;
     }
 
-int returns = 0;
+
     public int registerNode(int nodeId) {
-        // TODO 方法未实现
-    	try{
-    		if (nodeId<0||nodeId==0)
-    		{
-    			return ReturnCodeKeys.E004;
-    		}
-    		else{
-    			for(int i=0;i<nodelist.length;i++){
-    				if(nodeId==nodelist[i]){
-    					return ReturnCodeKeys.E005;
-    				}else{
-    					if (i==nodelist.length-1&&nodeId!=nodelist[i]){
-    						nodelist[i+1]=nodeId;
-    						return ReturnCodeKeys.E003;
-    					}
-    				}
-    			}
-    		}
-    	}
-    	catch(Exception e){
-    		return -1;
-    	}
-    	finally{
-    		if (nodeId<0||nodeId==0)
-    		{
-    			return ReturnCodeKeys.E004;
-    		}else{
-    			int i= nodelist.length-1;
-    			if (nodelist[i]==nodeId&&nodelist[i-1]!=nodeId){
-    				return ReturnCodeKeys.E003;
-    			}else {
-    				return ReturnCodeKeys.E005;
-    			}
-    		}
-    	}
-    	 
+        if (nodeId <= 0)
+        {
+            return ReturnCodeKeys.E004;
+        }
+        if (registerNodeSet.contains(nodeId))
+        {
+            return ReturnCodeKeys.E005;
+        }
+        registerNodeSet.add(nodeId);
+        return ReturnCodeKeys.E003;
     }
 
     public int unregisterNode(int nodeId) {
-        // TODO 方法未实现
-    	try{
-    		if (nodeId<0||nodeId==0)
-    		{
-    			return ReturnCodeKeys.E004;
-    		}
-    		else{
-    			for(int i = 0;i<nodelist.length;i++){
-    				if (nodelist[i]==nodeId){
-    					nodelist[i]=-1;
-    					Arrays.sort(nodelist);
-    					int temp = 0;
-    					for(int j=0;j<nodelist.length-1;j++){
-    						nodelist[i]=nodelist[i+1];
-    					}
-    					nodelist= Arrays.copyOf(nodelist, nodelist.length-1);
-    					return ReturnCodeKeys.E006;
-    				}else{
-    					if (i==nodelist.length-1&&nodelist[i-1]!=nodeId){
-    						return ReturnCodeKeys.E007;
-    					}
-    				}
-    			}
-    		}
-		}catch(Exception e){
-    		return -1;
-
-		}finally{
-			if (nodeId<0||nodeId==0)
-    		{
-    			return ReturnCodeKeys.E004;
-    		}
-    		else{
-    			int j=0;
-    			if ( j==nodelist.length-1&&nodelist[j-1]!=nodeId){
-					return ReturnCodeKeys.E007;
-				}else{
-					return ReturnCodeKeys.E006;
-
-				}
-    		}
-		}
-    	
+        if (nodeId <= 0)
+        {
+            return ReturnCodeKeys.E004;
+        }
+        if (!registerNodeSet.contains(nodeId))
+        {
+            return ReturnCodeKeys.E007;
+        }
+        LinkedList<Task> taskList = RuningTask.get(nodeId);
+        if(null != taskList && taskList.size()>0)
+        {
+            waitQueue.addAll(taskList);
+        }
+        registerNodeSet.remove(nodeId);
+        return ReturnCodeKeys.E006;
     }
 
 
     public int addTask(int taskId, int consumption) {
-        // TODO 方法未实现
-    	try{
-    		if (taskId<0||taskId==0){
-    			return ReturnCodeKeys.E009;
-    		}else{
-    			for(int i=0;i<tasklist.length;i++){
-    				if(taskId==tasklist[i]){
-    					return ReturnCodeKeys.E010;
-    				}else{
-    					if (i==tasklist.length-1&&taskId!=tasklist[i]){
-    						tasklist[i+1]=taskId;
-    						consumptionlist[i+1]=consumption;
-    						return ReturnCodeKeys.E008;
-    					}
-    				}
-    			}
-    		}
-    	}catch(Exception e){
-    		return -1;
-    	}
-    	finally{
-    		if (taskId<0||taskId==0)
-    		{
-    			return ReturnCodeKeys.E009;
-    		}else{
-    			int i= tasklist.length-1;
-    			if (tasklist[i]==taskId&&tasklist[i-1]!=taskId){
-    				return ReturnCodeKeys.E008;
-    			}else {
-    				return ReturnCodeKeys.E010;
-    			}
-    		}
-    	}
+        if (taskId <= 0)
+        {
+            return ReturnCodeKeys.E009;
+        }
+        for (Task task : waitQueue)
+        {
+            if (taskId == task.getTaskId())
+            {
+                return ReturnCodeKeys.E010;
+            }
+        }
+        
+        Task task = new Task(taskId, consumption);
+        waitQueue.add(task);
+        return ReturnCodeKeys.E008;
     }
 
 
     public int deleteTask(int taskId) {
-        // TODO 方法未实现
-    	try{
-    		if (taskId<0||taskId==0)
-    		{
-    			return ReturnCodeKeys.E009;
-    		}else{
-    			for(int i = 0;i<tasklist.length;i++){
-    				if (tasklist[i]==taskId){
-    					tasklist[i]=-1;
-    					Arrays.sort(tasklist);
-    					int temp = 0;
-    					for(int j=0;j<tasklist.length-1;j++){
-    						tasklist[i]=tasklist[i+1];
-    					}
-    					tasklist= Arrays.copyOf(tasklist, tasklist.length-1);
-    					return ReturnCodeKeys.E011;
-    				}else{
-    					if (i==tasklist.length-1&&tasklist[i-1]!=taskId){
-    						return ReturnCodeKeys.E012;
-    					}
-    				}
-    			}
-    		}
-    	}catch(Exception e){
-    		return -1;
-    	}finally{
-    		if (taskId<0||taskId==0)
-    		{
-    			return ReturnCodeKeys.E009;
-    		}
-    		else{
-    			int j=0;
-    			if ( j==tasklist.length-1&&tasklist[j-1]!=taskId){
-					return ReturnCodeKeys.E012;
-				}else{
-					return ReturnCodeKeys.E011;
-
-				}
-    		}
-    	}
+        if (taskId <= 0)
+        {
+            return ReturnCodeKeys.E009;
+        }
+        for (Task task : waitQueue)
+        {
+            if (taskId == task.getTaskId())
+            {
+                waitQueue.remove(task);
+                return ReturnCodeKeys.E011;
+            }
+        }
+        for (LinkedList<Task> tasks : RuningTask.values())
+        {
+            for (Task task : tasks)
+            {
+                if (taskId == task.getTaskId())
+                {
+                    tasks.remove(task);
+                    return ReturnCodeKeys.E011;
+                }
+            }
+        }
+        return ReturnCodeKeys.E012;
     }
 
 
     public int scheduleTask(int threshold) {
-        // TODO 方法未实现
+        int taskNum = 50/registerNodeSet.size();
+        if (waitQueue.size() < taskNum && waitQueue.size() > 0)
+        {
+            taskNum = waitQueue.size();
+        }
+        List<Task> taskPlanList = new ArrayList<Task>();
+        for (int i = 0; i < taskNum; i++)
+        {
+            Task task = waitQueue.poll();
+            taskPlanList.add(task);
+        }
+        
+        
         return ReturnCodeKeys.E000;
     }
 
 
     public int queryTaskStatus(List<TaskInfo> tasks) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        for (Task task : waitQueue)
+        {
+            TaskInfo taskinfo = new TaskInfo();
+            taskinfo.setTaskId(task.getTaskId());
+            taskinfo.setNodeId(-1);
+            tasks.add(taskinfo);
+        }
+        for (LinkedList<Task> taskList : RuningTask.values())
+        {
+            for (Task task : taskList)
+            {
+                TaskInfo taskinfo = new TaskInfo();
+                taskinfo.setTaskId(task.getTaskId());
+                taskinfo.setNodeId(-1);
+                tasks.add(taskinfo);
+            }
+        }
+        if (tasks == null || tasks.size() == 0)
+        {
+            return ReturnCodeKeys.E016;
+        }
+        return ReturnCodeKeys.E015;
     }
 
 }
